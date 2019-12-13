3a96ed0ee473c79e05a3f7a1cc9a24406cad2ffd
dahlia kadri <dahliakadri@gmail.com>
2019-12-12 21:03:58 -0800

    add js-trials updates

--- js-trials/arrays.js
+++ js-trials/arrays.js
@@ -1,32 +1,33 @@
-"use strict";
 
-const items = [1,2,4,6]
+const items = ['a', 'b', 'c'];
 
 /** 1. printIndices */
-function printIndices(items){
-// Loop over elements
-  for (const x in items) {
-    console.log(x, items[x]);
-  }
+function printIndices(items2) {
+	for (const i in items2) {
+    console.log(items2[i], i);
+  } 
 }
 
 
 /** 2. everyOtherItem */
-function everyOtherItem(items) {
-	// Replace this with your code
-  let result = [];
-
-  for (const x in items) {
-    if (x % 2 === 0) {
-      result.push(items[x]);
+function everyOtherItem(items3) {
+  const result = [];
+  for (const i in items3) {
+    if (i % 2 == 0) {
+      result.push(items3[i]);
     }
   }
   console.log(result);
 }
 
 
-/** 3. smallestNItems */
-function smallestNItems(items, n) {
-	// Replace this with your code
-}
+console.log(printIndices(items))
 
+console.log(everyOtherItem(items))
+
+
+
+/** 3. smallestNItems */
+function smallestNItems(items4) {
+  
+}
